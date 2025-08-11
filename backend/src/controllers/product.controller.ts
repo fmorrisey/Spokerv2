068699bdfff89f68fc98d95b7a48d100477e25bb
Forrest Morrisey <forrestmorrisey@gmail.com>
2025-08-11 09:07:06 -0500

    Swagger Gen and interface integration

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,21 +1,23 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, RequestHandler, Request, Response } from "express";
 import { Product } from "../models/product.model";
+import { ProductType } from '../../src/types/product.type';
 
-export const getAllProducts = async (req: Request, res: Response, next?: NextFunction) => {
+
+export const getAllProducts : RequestHandler<{}, ProductType[]> = async (req: Request, res: Response, next?: NextFunction) => {
   try {
-    const products = await Product.find();
+    const products = await Product.find().lean<ProductType[]>();
     res.status(200).json(products);
   } catch (error) {
     if (next) { next(error); }
     res.status(500).json({ error: `Error fetching products ${error}` });
   }
-}
+};
 
-export const createProduct = async (req: Request, res: Response, next?: NextFunction) => {
+export const createProduct : RequestHandler<{}, ProductType> = async (req: Request, res: Response, next?: NextFunction) => {
   try {
     const newProduct = new Product(req.body);
     const savedProduct = await newProduct.save();
-    res.status(201).json(savedProduct);
+    res.status(201).json(savedProduct.toObject());
   } catch (error) { 
     if (next) { next(error); }
     res.status(500).json({ error: `Error creating product ${error}` });
--- backend/src/routes/product.route.ts
+++ backend/src/routes/product.route.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { getAllProducts, createProduct } from "../controllers/product.controller";
+import { ProductType } from '../../src/types/product.type';
 
 
+// Define the router
 const router = express.Router();
 
-
-router.get("/", getAllProducts);
-router.post("/", createProduct);
+// Define the routes
+router.get<{}, ProductType[]>('/', getAllProducts);
+router.post<{}, ProductType>('/', createProduct);
 
 export default router;
