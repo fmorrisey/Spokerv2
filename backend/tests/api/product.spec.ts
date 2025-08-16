1379495615e82a09bde7cb4f4278d14429ea2395
Forrest Morrisey <forrestmorrisey@gmail.com>
2025-08-15 21:00:36 -0500

    Fixes and clean up

--- backend/tests/api/product.spec.ts
+++ backend/tests/api/product.spec.ts
@@ -46,8 +46,9 @@ describe('Product Controller', () => {
 
     await productController.getAllProducts(req, res, next);
 
-    expect(res.status).toHaveBeenCalledWith(Error);
-    expect(res.json).toHaveBeenCalledWith({ error: expect.stringContaining('Error fetching products') });
+    expect(next).toHaveBeenCalledWith(expect.any(Error));
+    expect(res.status).not.toHaveBeenCalled();
+    expect(res.json).not.toHaveBeenCalled();
   });
 
   it('should create a new product', async () => {
@@ -150,72 +151,19 @@ describe('Product Controller', () => {
     expect(res.status).toHaveBeenCalledWith(200);
     expect(res.json).toHaveBeenCalledWith(mockProduct);
   });
-
-  // Test get product by id
-  it('should fetch a product by id', async () => {
-    const mockProduct: ProductType = { _id: '1', name: 'Product 1', description: 'Description 1', msrp: 110, price: 100 };
-
-    jest.spyOn(ProductService, 'findById').mockResolvedValue(mockProduct as any);
-
-    const req: any = { params: { id: '1' } };
-    const res: any = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
-
-    await productController.getProductById(req, res, next);
-
-    expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith(mockProduct);
-  });
-
-  // test update product by id
-  it('should update a product by id', async () => {
-    const mockProduct: ProductType = { _id: '1', name: 'Product 1', description: 'Description 1', msrp: 110, price: 100 };
-    const updatedProductData: ProductType = { _id: '1', name: 'Updated Product 1', description: 'Updated Description 1', msrp: 120, price: 110 };
-
-    jest.spyOn(ProductService, 'findById').mockResolvedValue(mockProduct as any);
-    jest.spyOn(ProductService, 'updateById').mockResolvedValue(updatedProductData as any);
-
-    const req: any = { params: { id: '1' }, body: updatedProductData };
-    const res: any = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
-
-    await productController.updateProductById(req, res, next);
-
-    expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith(updatedProductData);
-  });
-
-  // test delete product by id
-  it('should delete a product by id', async () => {
-    const mockProduct: ProductType = { _id: '1', name: 'Product 1', description: 'Description 1', msrp: 110, price: 100 };
-
-    jest.spyOn(ProductService, 'findById').mockResolvedValue(mockProduct as any);
-    jest.spyOn(ProductService, 'deleteById').mockResolvedValue(mockProduct as any);
-
-    const req: any = { params: { id: '1' } };
-    const res: any = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-    const next = jest.fn();
-
-    await productController.deleteProductById(req, res, next);
-
-    expect(res.status).toHaveBeenCalledWith(200);
-    expect(res.json).toHaveBeenCalledWith(mockProduct);
-  });
 });
 
 describe('Product API', () => {
-  it('GET /api/products', async () => {
-    const res = await request(app).get(API_URL + Routes.PRODUCTS,);
+  it('GET /api/products and expect 200', async () => {
+    jest.spyOn(ProductService, 'findAll').mockResolvedValue([] as any);
+    const res = await request(app).get(API_URL + Routes.PRODUCTS);
     expect(res.status).toBe(200);
-    expect(res.body).toEqual(expect.any(Array));
+    expect(res.body).toEqual([]);
+  });
+
+  it('GET /api/productsbadroute and return 404', async () => {
+    jest.spyOn(ProductService, 'findAll').mockResolvedValue([] as any);
+    const res = await request(app).get(API_URL + Routes.PRODUCTS + 'badroute');
+    expect(res.status).toBe(404);
   });
 })
